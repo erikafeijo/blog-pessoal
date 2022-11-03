dbd6221c3691ce2875830228fc27f4b53477883c
lucasbrlos <feijoerika@gmail.com>
2022-11-03 10:31:27 -0300

    blog pessoal react

---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Home.css';
+
+function Home(){
+    return(
+        <>
+            <h1 className="titulo">Home</h1>
+            <img src="https://i.imgur.com/19ZIUzl.png" alt="Imagem Tela Inicial" width="1000" height="600"/>
+        </>
+    );
+}
+
+export default Home;
