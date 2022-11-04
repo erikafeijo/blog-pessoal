3002916e0b71f9cb8e80ecd522c0896cc858882d
lucasbrlos <feijoerika@gmail.com>
2022-11-04 16:04:25 -0300

    home footer navbar

---
+++ src/components/estaticos/footer/Footer.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+import InstagramIcon from '@material-ui/icons/Instagram';
+import FacebookIcon from '@material-ui/icons/Facebook';
+import WhatsAppIcon from '@material-ui/icons/WhatsApp';
+import { Box, Grid, Typography } from "@mui/material";
+import "./Footer.css"
+
+function Footer() {
+    return (
+        <>
+            <Grid container direction="row" justifyContent="center" alignItems="center">
+                <Grid alignItems="center" item xs={12}>
+                    <Box className="box1">
+                        <Box paddingTop={1} display="flex" alignItems="center" justifyContent="center">
+                            <Typography variant="h6" align="center" gutterBottom className="textos">Nossas redes sociais:</Typography>
+                        </Box>
+                        <Box display="flex" alignItems="center" justifyContent="center">
+                            <a href="https://www.facebook.com/generationbrasil" target="_blank">
+                                <FacebookIcon className="redes"/>
+                            </a>
+                            <a href="https://www.instagram.com/generationbrasil/" target="_blank">
+                                <InstagramIcon className="redes"/>
+                            </a>
+                            <a href="https://www.linkedin.com/sch6ool/generationbrasil/" target="_blank">
+                                <WhatsAppIcon className="redes"/>
+                            </a>
+                        </Box>
+                    </Box>
+                    <Box className="box2">
+                        <Box paddingTop={1}  display="flex" justifyContent="center" alignItems="center">
+                            <Typography variant="subtitle2" align="center" gutterBottom className="textos" >© 2022 Copyright:</Typography>
+                        </Box>
+                        <Box display="flex" justifyContent="center" alignItems="center">
+                                <Typography variant="subtitle2" gutterBottom className="textos" align="center">Amazonas de Gaia - Erika Feijo e Mariana Anjos</Typography>
+                        </Box>
+                    </Box>
+                </Grid>
+            </Grid>
+        </>
+    )
+}
+
+export default Footer;
--- src/paginas/home/Home.tsx
+++ src/paginas/home/Home.tsx
@@ -1,11 +1,29 @@
-import React from 'react';
-import './Home.css';
+import React from "react";
+import { Button, ButtonBaseActions} from "@material-ui/core"
+import "./Home.css";
+import { Box, Grid, Typography } from "@mui/material";
 
-function Home(){
-    return(
+function Home() {
+    return (
         <>
-            <h1 className="titulo">Home</h1>
-            <img src="https://i.imgur.com/19ZIUzl.png" alt="Imagem Tela Inicial" width="1000" height="600"/>
+            <Grid container direction="row" justifyContent="center" alignItems="center" className="caixa">
+                <Grid alignItems="center" item xs={6}>
+                    <Box paddingX={20} >
+                        <Typography variant="h3" gutterBottom color="textPrimary" component="h3" align="center" className="titulo">Seja bem vindo(a)!</Typography>
+                        <Typography variant="h5" gutterBottom color="textPrimary" component="h5" align="center" className="subtitulo">Expresse aqui os seus pensamentos e opiniões!</Typography>
+                    </Box>
+                    <Box display="flex" justifyContent="center">
+                        <Box marginRight={1}>
+                        </Box>
+                        <Button variant="outlined" className="botao">Ver Postagens</Button>
+                    </Box>
+                </Grid>
+                <Grid item xs={6} >
+                    <img src="https://i.imgur.com/19ZIUzl.png" alt="" width="500px" height="500px" />
+                </Grid>
+                <Grid xs={12} className="postagens">
+                </Grid>
+            </Grid>
         </>
     );
 }
