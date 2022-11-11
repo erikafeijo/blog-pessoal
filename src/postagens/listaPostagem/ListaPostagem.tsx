beac9304ae9e69581087b95e66653a09806fe286
lucasbrlos <feijoerika@gmail.com>
2022-11-11 00:18:34 -0300

    atualizando projeto

--- src/postagens/listaPostagem/ListaPostagem.tsx
+++ src/postagens/listaPostagem/ListaPostagem.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
-import Postagem from '../../../model/Postagem'
+import Postagem from '../../../Model/Postagem'
 import { busca } from '../../../service/Service'
 import { Card, CardActions, CardContent, Button, Typography } from '@material-ui/core';
 import './ListaPostagem.css';
--- src/service/Service.ts
+++ src/service/Service.ts
@@ -16,8 +16,29 @@ export const login = async(url: any, dados: any, setDados: any) => {
 }
 /*Como o back demora para enviar essa informação, temos que criar a função assíncrona para o front esperar*/
 
-export const busca = async(url: any, dados: any, setDados: any) => {
-    const resposta = await api.post(url, dados)
-    setDados(resposta.data.token)
-    /*vai ser usado pro lista tema e lista postagem*/
+/*A busca é um get, então não serão passados dados, só recebe dados. Além disso, precisamos colocar o token no header, como no insomnia*/
+export const busca = async (url: any, setDados: any, header: any) => {
+    const resposta = await api.get(url, header)
+    setDados(resposta.data)
+}
+/*Esse get vai pegar do back (api) o url e o header, para confirmar que o usuário está autenticado, armazenando a informação em resposta, e a
+i tendo esses dados, vai fornecer as informações de temas ou postagens que pedirmos */
+
+export const buscaId = async(url: any,setDados: any, header: any) => { 
+    const resposta = await api.get(url,header)
+    setDados(resposta.data)
+}
+
+export const post = async(url: any, dados: any, setDados: any, header: any) => { 
+    const resposta = await api.post(url,dados,header)
+    setDados(resposta.data)
+}
+
+export const put = async(url: any, dados: any, setDados: any, header: any) => { 
+    const resposta = await api.put(url,dados,header)
+    setDados(resposta.data)
+}
+
+export const deleteId = async(url: any,header: any) => { 
+    await api.delete(url,header)
 }
